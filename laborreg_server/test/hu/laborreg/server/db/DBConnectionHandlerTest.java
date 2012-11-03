e8700949115ebc470d045a69ff550bca1466abda
Zsolt Szilagyi <esgott@gmail.com>
2012-11-03 20:05:45 +0100

    mockito tried out in DBConnectionHandlerTest

--- laborreg_server/test/hu/laborreg/server/db/DBConnectionHandlerTest.java
+++ laborreg_server/test/hu/laborreg/server/db/DBConnectionHandlerTest.java
@@ -2,12 +2,26 @@ package hu.laborreg.server.db;
 
 import static org.junit.Assert.*;
 
+import java.sql.Connection;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 public class DBConnectionHandlerTest {
+	
+	@Mock
+	private Connection mockConnection;
+	
+	@Before
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+	}
 
 	@Test
 	public void test() {
+		DBConnectionHandler dbConnHandler = new DBConnectionHandler(mockConnection);
 		fail("Not yet implemented");
 	}
 
