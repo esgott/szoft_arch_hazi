fb4bf6fb0937aea4fa8ad499b83f1a4b9290d401
Gábor Pál <pal.gabor89@gmail.com>
2012-11-17 14:01:51 +0100

    Implementation of Course and CourseContainer classes

---
+++ laborreg_server/src/hu/laborreg/server/Course.java
@@ -0,0 +1,175 @@
+package hu.laborreg.server;
+
+import java.util.Set;
+
+public class Course {
+	
+	private String name;
+	private int year;
+	private Set<Student> registeredStudents;
+	private Set<LabEvent> labEvents;
+	
+	/**
+	 * This class represents the courses. The Lab leader can create and delete Courses, add and remove LabEvents to and from Courses, add and remove Students to and from Courses.
+	 * If the Lab leader doesn't add a Student to the Course Student list, then this Student will get an alarm when he or she sign in to the LabEvent (which allocated to the Course). 
+	 * @param name Name of the course.
+	 * @param year Year of the course.
+	 */
+	public Course(String name, int year)
+	{
+		this.name = name;
+		this.year = year;
+	}
+	
+	/**
+	 * Returns the name of the course.
+	 * @return The name of the course.
+	 */
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	/**
+	 * Returns the year of the course.
+	 * @return The year of the course.
+	 */
+	public int getYear()
+	{
+		return this.year;
+	}
+	
+	/**
+	 * Returns the registered students of this Course.
+	 * @return The registered students.
+	 */
+	public Set<Student> getRegisteredStudents()
+	{
+		return this.registeredStudents;
+	}
+	
+	/**
+	 * Returns the existing lab events of this Course
+	 * @return The existing lab events.
+	 */
+	public Set<LabEvent> getLabEvents()
+	{
+		return this.labEvents;
+	}
+	
+	/**
+	 * Register Student to this Course.
+	 * @param student The Student who wants to be registered to this Course. 
+	 */
+	public void registerStudent(Student student)
+	{
+		try
+		{
+			if(registeredStudents.add(student) == false)
+			{
+				//TODO Student is already member of this course.  Display error in error window?
+			}
+		}
+		catch(UnsupportedOperationException e)
+		{
+			e.printStackTrace();
+		}
+		catch(ClassCastException e)
+		{
+			e.printStackTrace();
+		}
+		catch(NullPointerException e)
+		{
+			e.printStackTrace();
+		}
+		catch(IllegalArgumentException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Delete Student from this Course.
+	 * @param student The Student who wants to be deleted from this Course.
+	 */
+	public void unregisterStudent(Student student)
+	{
+		try
+		{
+			if(registeredStudents.remove(student) == false)
+			{
+				//TODO Student doesn't registered to this course. Display error in error window?
+			}
+		}
+		catch(UnsupportedOperationException e)
+		{
+			e.printStackTrace();
+		}
+		catch(ClassCastException e)
+		{
+			e.printStackTrace();
+		}
+		catch(NullPointerException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Add lab event to this course.
+	 * @param labEvent The lab event which want to be added to this Course.
+	 */
+	public void addLabEvent(LabEvent labEvent)
+	{
+		try
+		{
+			if(labEvents.add(labEvent) == false)
+			{
+				//TODO LabEvent is already member of this course. Display error in error window?
+			}
+		}
+		catch(UnsupportedOperationException e)
+		{
+			e.printStackTrace();
+		}
+		catch(ClassCastException e)
+		{
+			e.printStackTrace();
+		}
+		catch(NullPointerException e)
+		{
+			e.printStackTrace();
+		}
+		catch(IllegalArgumentException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Remove lab event from this Course.
+	 * @param labEvent The lab event which want to be removed from this Course.
+	 */
+	public void removeLabEvent(LabEvent labEvent)
+	{
+		try
+		{
+			if(labEvents.remove(labEvent) == false)
+			{
+				//TODO LabEvent doesn't member of this course. Display error in error window?
+			}
+		}
+		catch(UnsupportedOperationException e)
+		{
+			e.printStackTrace();
+		}
+		catch(ClassCastException e)
+		{
+			e.printStackTrace();
+		}
+		catch(NullPointerException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
--- laborreg_server/src/hu/laborreg/server/CourseContainer.java
+++ laborreg_server/src/hu/laborreg/server/CourseContainer.java
@@ -1,5 +1,98 @@
 package hu.laborreg.server;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class CourseContainer {
-	//TODO
+	
+	private Set<Course> courses;
+	
+	/**
+	 * A container class which contains the courses.
+	 */
+	public CourseContainer()
+	{
+		//TODO read the existing courses from the DB.
+	}
+	
+	/**
+	 * Add course to the courses list.
+	 * @param course
+	 */
+	public void addCourse(Course course)
+	{
+		
+		try
+		{
+			if(this.courses.add(course) == false)
+			{
+				//TODO courses already contain this course. Display error in error window?
+			}
+		}
+		catch(UnsupportedOperationException e)
+		{
+			e.printStackTrace();
+		}
+		catch(ClassCastException e)
+		{
+			e.printStackTrace();
+		}
+		catch(NullPointerException e)
+		{
+			e.printStackTrace();
+		}
+		catch(IllegalArgumentException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Remove course to the courses list.
+	 * @param course
+	 */
+	public void removeCourse(Course course)
+	{
+		
+		try
+		{
+			if(this.courses.remove(course) == false)
+			{
+				//TODO courses doesn't contain this course.  Display error in error window?
+			}
+		}
+		catch(UnsupportedOperationException e)
+		{
+			e.printStackTrace();
+		}
+		catch(ClassCastException e)
+		{
+			e.printStackTrace();
+		}
+		catch(NullPointerException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Get the specified course from courses.
+	 * @param name The name of the course (must be unique).
+	 * @param year The year of the course.
+	 * @return The needed course
+	 */
+	public Course getCourse(String name, int year)
+	{
+		Iterator<Course> it = courses.iterator();
+		
+		while(it.hasNext())
+		{
+			Course retVal = it.next();
+			if(retVal.getName().equals(name) && retVal.getYear()==year)
+			{
+				return retVal;
+			}
+		}
+		return null;
+	}
 }
