8e1c37899a70e1fced94a9dcdd3a1f706056daff
Zsolt Szilagyi <esgott@gmail.com>
2012-11-17 17:41:55 +0100

    added HttpFileHandlerTest to AllTests, and moved to test folder

--- laborreg_server/test/hu/laborreg/server/AllTests.java
+++ laborreg_server/test/hu/laborreg/server/AllTests.java
@@ -2,6 +2,7 @@ package hu.laborreg.server;
 
 import hu.laborreg.server.db.DBConnectionHandlerTest;
 import hu.laborreg.server.db.DBInitiatorTest;
+import hu.laborreg.server.http.HttpFileHandlerTest;
 import hu.laborreg.server.http.HttpRequestListenerThreadTest;
 import hu.laborreg.server.http.HttpWorkerThreadTest;
 
@@ -11,7 +12,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ DBConnectionHandlerTest.class, DBInitiatorTest.class, HttpWorkerThreadTest.class,
-		HttpRequestListenerThreadTest.class })
+		HttpRequestListenerThreadTest.class, HttpFileHandlerTest.class })
 public class AllTests {
 
 }
