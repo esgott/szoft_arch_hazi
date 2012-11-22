f012e62fbdc1753917600ce73a9595c1666312ed
Zsolt Szilagyi <esgott@gmail.com>
2012-11-22 18:13:20 +0100

    added clean and setFields functions to LabEventManiupulatorPanel

--- laborreg_server/src/hu/laborreg/server/gui/LabEventManipulatorPanel.java
+++ laborreg_server/src/hu/laborreg/server/gui/LabEventManipulatorPanel.java
@@ -1,8 +1,11 @@
 package hu.laborreg.server.gui;
+
+import hu.laborreg.server.labEvent.LabEvent;
+
 import java.awt.GridLayout;
+
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-
 
 public class LabEventManipulatorPanel extends ManipulatorPanel {
 
@@ -12,41 +15,42 @@ public class LabEventManipulatorPanel extends ManipulatorPanel {
 	private JTextField courseYearTextField;
 	private JTextField startTextField;
 	private JTextField endTextField;
+	private LabEvent oldLabEvent;
 
 	public LabEventManipulatorPanel() {
 		setLayout(new GridLayout(5, 2, 10, 10));
-		
+
 		JLabel nameLabel = new JLabel("Név");
 		add(nameLabel);
-		
+
 		nameTextField = new JTextField();
 		add(nameTextField);
 		nameTextField.setColumns(10);
-		
+
 		JLabel courseNameLabel = new JLabel("Kurzus név:");
 		add(courseNameLabel);
-		
+
 		courseNameTextField = new JTextField();
 		add(courseNameTextField);
 		courseNameTextField.setColumns(10);
-		
+
 		JLabel courseYearLabel = new JLabel("Kurzus év:");
 		add(courseYearLabel);
-		
+
 		courseYearTextField = new JTextField();
 		add(courseYearTextField);
 		courseYearTextField.setColumns(10);
-		
+
 		JLabel startLabel = new JLabel("Kezdés:");
 		add(startLabel);
-		
+
 		startTextField = new JTextField();
 		add(startTextField);
 		startTextField.setColumns(10);
-		
+
 		JLabel endlabel = new JLabel("Befejezés:");
 		add(endlabel);
-		
+
 		endTextField = new JTextField();
 		add(endTextField);
 		endTextField.setColumns(10);
@@ -55,14 +59,22 @@ public class LabEventManipulatorPanel extends ManipulatorPanel {
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		nameTextField.setText("");
+		courseNameTextField.setText("");
+		courseYearTextField.setText("");
+		startTextField.setText("");
+		endTextField.setText("");
+	}
+	
+	public void setFields(LabEvent labEvent) {
+		oldLabEvent = labEvent;
+		//TODO set fields
 	}
 
 	@Override
 	public void commit() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
