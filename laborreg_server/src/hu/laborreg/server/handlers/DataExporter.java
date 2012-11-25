14142ca25602f0d9a84ffadf765086646fb20949
Gábor Pál <pal.gabor89@gmail.com>
2012-11-25 19:30:04 +0100

    DataExporter now can export Sign in data

--- laborreg_server/src/hu/laborreg/server/handlers/DataExporter.java
+++ laborreg_server/src/hu/laborreg/server/handlers/DataExporter.java
@@ -15,35 +15,58 @@ import hu.laborreg.server.db.DBConnectionHandler;
 import hu.laborreg.server.labEvent.LabEvent;
 
 public class DataExporter {
-	
+
 	private DBConnectionHandler dbConnHandler;
 	private final Logger logger = Logger.getLogger(this.getClass().getName());
-	
+
 	/**
-	 * Enables to save the LabEvents' data to a hard drive. It reads the data of the LabEvents (after it is finished) from the database, then save the data to the specified path in CSV format.
-	 * @param dbConnHandler Handler of the database connection.
+	 * Enables to save the LabEvents' data to a hard drive. It reads the data of
+	 * the LabEvents (after it is finished) from the database, then save the
+	 * data to the specified path in CSV format.
+	 * 
+	 * @param dbConnHandler
+	 *            Handler of the database connection.
 	 */
-	public DataExporter(DBConnectionHandler dbConnHandler)
-	{
+	public DataExporter(DBConnectionHandler dbConnHandler) {
 		this.dbConnHandler = dbConnHandler;
 	}
 
-	public void exportData(File file)
-	{	
-		try{
-			CSVWriter writer = new CSVWriter(new FileWriter(file.getPath() + ".csv"), ',');
+	public void exportData(File file) {
+		try {
+			CSVWriter writer = new CSVWriter(new FileWriter(file.getPath()
+					+ ".csv"), ',');
 			ResultSet result;
 			try {
 				PreparedStatement statement = dbConnHandler.createPreparedStatement("SELECT * FROM lab_event");
 				result = statement.executeQuery();
-				writer.writeAll(result,true);
+				
+				int nrOfColumns = result.getMetaData().getColumnCount();  
+				
+				while (result.next()) {
+					String lineOfCSV = new String();
+					for(int i=0;i<nrOfColumns;i++)
+					{
+						lineOfCSV += result.getString(i+1);
+						lineOfCSV += "###";
+					}
+					
+					String labEventName = result.getString("name");
+					PreparedStatement statementOfSignedTable = dbConnHandler.createPreparedStatement("SELECT * FROM signed WHERE lab_event_name = ?");
+					statementOfSignedTable.setString(1, labEventName);
+					ResultSet resultOfSignedTable = statementOfSignedTable.executeQuery();
+					
+					while(resultOfSignedTable.next()) {
+						lineOfCSV += resultOfSignedTable.getString(2);
+						lineOfCSV += "###";
+					}
+				     writer.writeNext(lineOfCSV.split("###"));
+				}
 			} catch (SQLException e) {
 				logger.severe("Failed to read LabEvents from DB: " + e.getMessage());
-			}	
+			}
 			writer.close();
-			
-		}catch (IOException e)
-		{
+
+		} catch (IOException e) {
 			logger.severe("Failed to write LabEvents to the CSV file: " + e.getMessage());
 		}
 	}
